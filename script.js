eadf198b3f4a45170f86ff6fc457829402347301
Juanfrina <jfco11092024@outlook.com>
2025-10-10 12:07:59 +0200

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript muy básico para el portafolio
+// Solo funciones simples que puedes entender fácilmente
+
+// Esta línea hace que la página espere a cargar antes de ejecutar el código
+window.onload = function() {
+    
+    // Mensaje de bienvenida en la consola (puedes verlo en F12)
+    console.log("¡Portafolio de Juan Francisco cargado correctamente!");
+    
+};
